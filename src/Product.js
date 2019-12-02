4b67823676fdadc0993cca95356fc9691b630fdc
Bhagyashree Mahajan <bhagyashreemahajan123@gmail.com>
2019-12-01 21:17:12 -0800

    Update2

--- src/Product.js
+++ src/Product.js
@@ -5,12 +5,12 @@ import ProductTable from './ProductTable';
 import ProductForm from './ProductForm';
 
 let PRODUCTS = {
-    '1': {id: 1, category: 'Sports', price: '$99.99', name: 'Cricket-Bat'},
-    '2': {id: 2, category: 'Sports', price: '$50', name: 'Ball'},
-    '3': {id: 3, category: 'Sports', price: '$19.99', name: 'Gloves'},
-    '4': {id: 4, category: 'Electronics', price: '$799', name: 'Samsung Note 10'},
-    '5': {id: 5, category: 'Electronics', price: '$699', name: 'TV 65inch'},
-    '6': {id: 6, category: 'Electronics', price: '$999', name: 'Sony PlayStation'}
+    '1': {id: 1, category: 'Music', price: '$459.99', name: 'Clarinet'},
+    '2': {id: 2, category: 'Music', price: '$5,000', name: 'Cello'},
+    '3': {id: 3, category: 'Music', price: '$4,500', name: 'Tuba'},
+    '4': {id: 4, category: 'Furniture', price: '$799', name: 'Chaise Lounge'},
+    '5': {id: 5, category: 'Furniture', price: '$1,300', name: 'Dining Table'},
+    '6': {id: 6, category: 'Furniture', price: '$100', name: 'Bean Bag'}
 };
 
 class Product extends Component {
